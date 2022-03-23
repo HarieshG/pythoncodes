822ad19bd92bcddde88c989f47ccfeb026430d18
HarieshG <harieshragavendra@gmail.com>
2022-03-23 18:22:55 +0530

    fillzero1

--- join_1.py
+++ join_1.py
@@ -4,6 +4,21 @@ from pyspark.sql import SparkSession
 from pyspark.sql.functions import count, when,col,expr, udf
 from  pyspark.sql.types import IntegerType
 
+# -----------------------------FUNCTIONS-----------------------------------------------
+
+#function to summation
+def summation(*arguments):
+    total = 0
+    for number in arguments:
+        total += number
+    return total
+
+#function for displaying NULL Count
+def displayNullCount(df):
+    df.select([count(when(col(c).isNull(), c)).alias(c) for c in df.columns]).show()
+
+#-----------------------------CODE---------------------------------------------------
+
 STORAGEACCOUNTURL = "https://trainingbatchaccount.blob.core.windows.net"
 STORAGEACCOUNTKEY = "2QPPHsAtQ8/fh33VE7wqg/ZaeJoxdq/pnevAEmCh0n32tC5eXa8dTEEwMHdD9Ff5k1/wVh97aubqgKzQSwOLnQ=="
 CONTAINERNAME = "datasets"
@@ -25,21 +40,9 @@ df_geo = df_geo.na.fill(0, subset=['area_sq_km','area_rural_sq_km','area_urban_s
 #Reading demographics dataset
 df_demo = spark.read.format('csv').option('header',True).option('inferSchema',True).load("wasbs://datasets@trainingbatchaccount.blob.core.windows.net/demographics.csv")
 
-#function to summation
-def summation(*arguments):
-    total = 0
-    for number in arguments:
-        total += number
-    return total
+#fill null population with sum of male & female population
+addpopulation = when(col("population").isNull(), (col("population_male") + col("population_female"))).otherwise(col("population"))
+df_demo = df_demo.withColumn("population", addpopulation)
 
-def displayNullCount(df):
-    df.select([count(when(col(c).isNull(), c)).alias(c) for c in df.columns]).show()
-
-new_column_2 = when(col("population").isNull(), (col("population_male") + col("population_female"))).otherwise(col("population"))
-# new_func = udf(summation, IntegerType())
-# print(new_func('population_male','population_female'))
-#Replace null population with male and female pop
-# df_demo = df_demo.na.fill(new_func('population_male','population_female'), subset=['population'])
-df_demo = df_demo.withColumn("population", new_column_2)
-displayNullCount(df_demo)
-# df_demo.show()
+#Fill null values with zero
+df_demo = df_demo.na.fill(0, subset=['population_rural', 'population_urban','population_largest_city','population_largest_city','population_density','human_development_index',	'population_age_00_09','population_age_10_19',	'population_age_20_29',	'population_age_30_39',	'population_age_40_49'	,'population_age_50_59'	,'population_age_60_69',	'population_age_70_79'	,'population_age_80_and_older'])
