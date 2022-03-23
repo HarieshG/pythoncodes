f4181b0add6f309a724be9f21f4a6990b044d9ff
HarieshG <harieshragavendra@gmail.com>
2022-03-23 11:36:48 +0530

    c1

--- join_1.py
+++ join_1.py
@@ -1,5 +1,14 @@
 import pyspark
 from pyspark.sql import SparkSession
 
+STORAGEACCOUNTURL = "https://trainingbatchaccount.blob.core.windows.net"
+STORAGEACCOUNTKEY = "2QPPHsAtQ8/fh33VE7wqg/ZaeJoxdq/pnevAEmCh0n32tC5eXa8dTEEwMHdD9Ff5k1/wVh97aubqgKzQSwOLnQ=="
+CONTAINERNAME = "datasets"
+
 spark = SparkSession.builder.appName('Join_1').getOrCreate()
-print(spark)
+spark.conf.set(
+        "fs.azure.account.key.trainingbatchaccount.blob.core.windows.net",
+        STORAGEACCOUNTKEY
+)
+sdf = spark.read.format('csv').option('header',True).option('inferSchema',True).load("wasbs://datasets@trainingbatchaccount.blob.core.windows.net/demographics.csv")
+sdf.show()
