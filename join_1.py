c79095ef176f7baf521efecc7af91e39e8cae2ef
HarieshG <harieshragavendra@gmail.com>
2022-03-23 15:45:22 +0530

    fillzero1

--- join_1.py
+++ join_1.py
@@ -32,10 +32,14 @@ def summation(*arguments):
         total += number
     return total
 
+def displayNullCount(df):
+    df.select([count(when(col(c).isNull(), c)).alias(c) for c in df.columns]).show()
+
 new_column_2 = when(col("population").isNull(), (col("population_male") + col("population_female"))).otherwise(col("population"))
 # new_func = udf(summation, IntegerType())
 # print(new_func('population_male','population_female'))
 #Replace null population with male and female pop
 # df_demo = df_demo.na.fill(new_func('population_male','population_female'), subset=['population'])
-df_demo.withColumn("population", new_column_2).show()
+df_demo = df_demo.withColumn("population", new_column_2)
+displayNullCount(df_demo)
 # df_demo.show()
