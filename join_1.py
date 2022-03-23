8c0eac13a834698b46b6c4b81560f8f823ee4c77
HarieshG <harieshragavendra@gmail.com>
2022-03-23 13:19:14 +0530

    fillzero

--- join_1.py
+++ join_1.py
@@ -20,7 +20,7 @@ df_geo = spark.read.format('csv').option('header',True).option('inferSchema',Tru
 df_geo = df_geo.drop('elevation_m','openstreetmap_id')
 
 #Fill null values with zero
-df_geo = df_geo.na.fill(0, subset=['area_sq_km','area_rural_sq_km','area_urban_sq_km'])
+df_geo = df_geo.na.fill(0, subset=['area_sq_km','area_rural_sq_km','area_urban_sq_km','lattitude','longitude'])
 
 #Check null values exist
 df_geo.select([count(when(col(c).isNull(), c)).alias(c) for c in df_geo.columns]).show()
