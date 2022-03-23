cb2d73dc14ead400ef5a3865bf5158c50aa10f84
HarieshG <harieshragavendra@gmail.com>
2022-03-23 18:27:17 +0530

    fillzero1

--- join_1.py
+++ join_1.py
@@ -46,6 +46,6 @@ addpopulation = when(col("population").isNull(), (col("population_male") + col("
 df_demo = df_demo.withColumn("population", addpopulation)
 
 #Fill null values with zero
-df_demo = df_demo.na.fill(0, subset=['population_rural', 'population_urban','population_largest_city','population_largest_city','population_density','human_development_index',	'population_age_00_09','population_age_10_19',	'population_age_20_29',	'population_age_30_39',	'population_age_40_49'	,'population_age_50_59'	,'population_age_60_69',	'population_age_70_79'	,'population_age_80_and_older'])
+df_demo = df_demo.na.fill(0, subset=['population_rural', 'population_clustered','population_urban','population_largest_city','population_largest_city','population_density','human_development_index',	'population_age_00_09','population_age_10_19',	'population_age_20_29',	'population_age_30_39',	'population_age_40_49'	,'population_age_50_59'	,'population_age_60_69',	'population_age_70_79'	,'population_age_80_and_older'])
 
 displayNullCount(df_demo)
