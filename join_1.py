452cedef6a170ed630bbd2b67f0739374656542b
HarieshG <harieshragavendra@gmail.com>
2022-03-24 12:43:00 +0530

    epido

--- join_1.py
+++ join_1.py
@@ -109,7 +109,7 @@ df_epidemiology = df_epidemiology.withColumn('date',to_date(df_epidemiology['dat
 
 df_epidemiology.printSchema()
 
-df_epidemiology.set_index('date')['Value'].interpolate(method="linear")
+df_epidemiology['new_confirmed'].interpolate(method="linear")
 
 displayNullCount(df_epidemiology)
 
