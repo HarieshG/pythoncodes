a1f3008ac4618ab7734b9d0bcf643e5dd4250798
HarieshG <harieshragavendra@gmail.com>
2022-03-24 22:43:53 +0530

    epido

--- join_1.py
+++ join_1.py
@@ -41,66 +41,66 @@ spark.conf.set(
         "fs.azure.account.key.trainingbatchaccount.blob.core.windows.net",
         STORAGEACCOUNTKEY
 )
-# #----------------------Geography---------------------
+#----------------------Geography---------------------
 
-# #Reading geography dataset
-# df_geo = spark.read.format('csv').option('header',True).option('inferSchema',True).load("wasbs://datasets@trainingbatchaccount.blob.core.windows.net/geography.csv")
+#Reading geography dataset
+df_geo = spark.read.format('csv').option('header',True).option('inferSchema',True).load("wasbs://datasets@trainingbatchaccount.blob.core.windows.net/geography.csv")
 
-# #Drop openstreetmapid & elevation_m
-# df_geo = df_geo.drop('elevation_m','openstreetmap_id')
+#Drop openstreetmapid & elevation_m
+df_geo = df_geo.drop('elevation_m','openstreetmap_id')
 
-# #Fill null values with zero
-# df_geo = df_geo.na.fill(0, subset=['area_sq_km','area_rural_sq_km','area_urban_sq_km','latitude','longitude'])
+#Fill null values with zero
+df_geo = df_geo.na.fill(0, subset=['area_sq_km','area_rural_sq_km','area_urban_sq_km','latitude','longitude'])
 
-# #----------------------Demographics---------------------
+#----------------------Demographics---------------------
 
-# #Reading demographics dataset
-# df_demo = spark.read.format('csv').option('header',True).option('inferSchema',True).load("wasbs://datasets@trainingbatchaccount.blob.core.windows.net/demographics.csv")
+#Reading demographics dataset
+df_demo = spark.read.format('csv').option('header',True).option('inferSchema',True).load("wasbs://datasets@trainingbatchaccount.blob.core.windows.net/demographics.csv")
 
-# #fill null population with sum of male & female population
-# addpopulation = when(col("population").isNull(), (col("population_male") + col("population_female"))).otherwise(col("population"))
-# df_demo = df_demo.withColumn("population", addpopulation)
+#fill null population with sum of male & female population
+addpopulation = when(col("population").isNull(), (col("population_male") + col("population_female"))).otherwise(col("population"))
+df_demo = df_demo.withColumn("population", addpopulation)
 
-# #Fill null values with zero
-# df_demo = df_demo.na.fill(0, subset=['population_rural', 'population_clustered','population_urban','population_largest_city','population_largest_city','population_density','human_development_index',	'population_age_00_09','population_age_10_19',	'population_age_20_29',	'population_age_30_39',	'population_age_40_49'	,'population_age_50_59'	,'population_age_60_69',	'population_age_70_79'	,'population_age_80_and_older'])
+#Fill null values with zero
+df_demo = df_demo.na.fill(0, subset=['population_rural', 'population_clustered','population_urban','population_largest_city','population_largest_city','population_density','human_development_index',	'population_age_00_09','population_age_10_19',	'population_age_20_29',	'population_age_30_39',	'population_age_40_49'	,'population_age_50_59'	,'population_age_60_69',	'population_age_70_79'	,'population_age_80_and_older'])
 
-# #sum of rows
-# df_value = df_demo.agg(avg(df_demo.population_male), avg(df_demo.population_female)).collect()
-# v_avg = df_value[0][0]/df_value[0][1]
-# ratio = v_avg.as_integer_ratio()
-# div_value = ratio[0] + ratio[1]
+#sum of rows
+df_value = df_demo.agg(avg(df_demo.population_male), avg(df_demo.population_female)).collect()
+v_avg = df_value[0][0]/df_value[0][1]
+ratio = v_avg.as_integer_ratio()
+div_value = ratio[0] + ratio[1]
 
-# #fill null population_male 
-# addpopulation_male = when(col("population_male").isNull(), (col("population") / div_value)*ratio[0]).otherwise(col("population_male"))
-# df_demo = df_demo.withColumn("population_male", addpopulation_male)
+#fill null population_male 
+addpopulation_male = when(col("population_male").isNull(), (col("population") / div_value)*ratio[0]).otherwise(col("population_male"))
+df_demo = df_demo.withColumn("population_male", addpopulation_male)
 
-# #fill null population_female 
-# addpopulation_female = when(col("population_female").isNull(), (col("population") / div_value)*ratio[1]).otherwise(col("population_female"))
-# df_demo = df_demo.withColumn("population_female", addpopulation_female)
+#fill null population_female 
+addpopulation_female = when(col("population_female").isNull(), (col("population") / div_value)*ratio[1]).otherwise(col("population_female"))
+df_demo = df_demo.withColumn("population_female", addpopulation_female)
 
-# #----------------------Join_1---------------------
-# df_join_1 = df_demo.join(df_geo, df_geo.location_key == df_demo.location_key,'inner').drop(df_demo.location_key)
+#----------------------Join_1---------------------
+df_join_1 = df_demo.join(df_geo, df_geo.location_key == df_demo.location_key,'inner').drop(df_demo.location_key)
 
-# #----------------------Economy---------------------
-# df_eco = spark.read.format('csv').option('header',True).option('inferSchema',True).load("wasbs://datasets@trainingbatchaccount.blob.core.windows.net/economy.csv")
+#----------------------Economy---------------------
+df_eco = spark.read.format('csv').option('header',True).option('inferSchema',True).load("wasbs://datasets@trainingbatchaccount.blob.core.windows.net/economy.csv")
 
-# #fill null with zero
-# df_eco = df_eco.na.fill(0)
+#fill null with zero
+df_eco = df_eco.na.fill(0)
 
-# #----------------------Join_2---------------------
-# df_join_2 = df_join_1.join(df_eco, df_eco.location_key == df_join_1.location_key,'inner').drop(df_eco.location_key)
+#----------------------Join_2---------------------
+df_join_2 = df_join_1.join(df_eco, df_eco.location_key == df_join_1.location_key,'inner').drop(df_eco.location_key)
 
-# df_join_2.printSchema()
+df_join_2.printSchema()
 
-# #----------------------Health---------------------
-# df_health = spark.read.format('csv').option('header',True).option('inferSchema',True).load("wasbs://datasets@trainingbatchaccount.blob.core.windows.net/health.csv")
+#----------------------Health---------------------
+df_health = spark.read.format('csv').option('header',True).option('inferSchema',True).load("wasbs://datasets@trainingbatchaccount.blob.core.windows.net/health.csv")
 
-# # filling the null values in the life_expectancy column with the mean of it
-# imputer = Imputer(inputCol='life_expectancy',outputCol="life_expectancy").setStrategy("mean")
-# health_df = imputer.fit(df_health).transform(df_health)
+# filling the null values in the life_expectancy column with the mean of it
+imputer = Imputer(inputCol='life_expectancy',outputCol="life_expectancy").setStrategy("mean")
+health_df = imputer.fit(df_health).transform(df_health)
 
-# #----------------------Join_3---------------------
-# df_join_3 = df_join_2.join(df_health, df_health.location_key == df_join_2.location_key,'inner').drop(df_health.location_key)
+#----------------------Join_3---------------------
+df_join_3 = df_join_2.join(df_health, df_health.location_key == df_join_2.location_key,'inner').drop(df_health.location_key)
 
 
 #----------------------Epidemiology---------------------
@@ -109,9 +109,7 @@ df_epidemiology = spark.read.format('csv').option('header',True).option('inferSc
 df_epidemiology = df_epidemiology.withColumn('date',to_date(df_epidemiology['date'],format='yyyy-mm-dd'))
 
 df_epidemiology = df_epidemiology.na.fill(value=0)
-displayNullCount(df_epidemiology)
-
-
+displayNullCount(df_join_3)
 
 
 
